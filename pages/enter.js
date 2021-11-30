5a23062b451a830caa88cc14117d10d8840cde2d
Noevenzahn <info.artuofficial@gmail.com>
2021-11-30 18:30:38 +0100

    add authentication

---
+++ pages/enter.js
@@ -0,0 +1,44 @@
+import { auth, googleAuthProvider } from '../lib/firebase';
+import Image from 'next/dist/client/image';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
+
+export default function Enter(props) {
+
+  const { user, username } = useContext(UserContext)
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
+  return (
+    <main>
+      {user ? 
+        !username ? <UsernameForm /> : <SignOutButton /> 
+        : 
+        <SignInButton />
+      }
+    </main>
+  );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <Image src={'/google-logo.svg'} alt="" width={35} height={35} /> Sign in with Google
+    </button>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
+}
